32e4567327b61ad281023a14e01eb7d9afa1ad13
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-09 10:24:59 +0000

    Update seed data to use dynamic dates for subscriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,10 @@ user2 = User.create!(email_address: 'test@test.com', password: 'password', first
 puts "#{User.count} users created"
 
 # create 2 test subscriptions per user
-user1.subscriptions.create!(name: 'Subscription 1', price: "9.99", billing_frequency: 'monthly', started_at: '2025-01-01 10:23:38', expires_at: '2025-05-01 10:23:38')
-user1.subscriptions.create!(name: 'Subscription 2', price: "19.99", billing_frequency: 'monthly', started_at: '2024-01-01 10:23:38', expires_at: '2024-05-01 10:23:38')
+user1.subscriptions.create!(name: 'Subscription 1', price: "9.99", billing_frequency: 'monthly', started_at: Time.current - 1.month, expires_at: Time.current + 1.month)
+user1.subscriptions.create!(name: 'Subscription 2', price: "19.99", billing_frequency: 'monthly', started_at: Time.current - 2.months, expires_at: Time.current - 1.month)
 
-user2.subscriptions.create!(name: 'Subscription 3', price: "29.99", billing_frequency: 'monthly', started_at: '2023-01-01 10:23:38', expires_at: '2023-05-01 10:23:38')
-user2.subscriptions.create!(name: 'Subscription 4', price: "39.99", billing_frequency: 'monthly', started_at: '2022-01-01 10:23:38', expires_at: '2022-05-01 10:23:38')
+user2.subscriptions.create!(name: 'Subscription 3', price: "29.99", billing_frequency: 'monthly', started_at: Time.current - 1.month, expires_at: Time.current + 1.month)
+user2.subscriptions.create!(name: 'Subscription 4', price: "39.99", billing_frequency: 'monthly', started_at: Time.current - 2.months, expires_at: Time.current - 1.month)
 
 puts "#{Subscription.count} subscriptions created"
