a9b0300ff2bebc11df661127fc497217f2d7c0c5
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-10 10:40:15 +0000

    Add subscription show page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,4 +2,8 @@ class SubscriptionsController < ApplicationController
   def index
     @subscriptions = Current.user.subscriptions
   end
+
+  def show
+    @subscription = Current.user.subscriptions.find(params[:id])
+  end
 end
