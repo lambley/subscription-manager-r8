72a7c46eeb65d91e346f3692fd29b19b361d3661
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-14 11:05:29 +0000

    Refactor subscription card content into new partial for show and index views

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,9 +1,9 @@
 module SubscriptionsHelper
-  def add_is_active_badge(is_active)
-    if is_active
-      content_tag(:span, "Active", class: "bg-green-500 text-white px-2 py-1 rounded-full")
+  def subscription_status(subscription)
+    if subscription.active?
+      content_tag(:span, "Active", class: "bg-green-500 text-white px-4 py-1 rounded-full")
     else
-      content_tag(:span, "Inactive", class: "bg-red-500 text-white px-2 py-1 rounded-full")
+      content_tag(:span, "Inactive", class: "bg-red-500 text-white px-4 py-1 rounded-full")
     end
   end
 end
