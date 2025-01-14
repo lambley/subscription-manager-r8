88a8dd2a80a94c84965a0beca7ae098fa09f9299
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-14 09:46:35 +0000

    Add edit and update actions for subscriptions with corresponding views

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,4 +6,23 @@ class SubscriptionsController < ApplicationController
   def show
     @subscription = Current.user.subscriptions.find(params[:id])
   end
+
+  def edit
+    @subscription = Current.user.subscriptions.find(params[:id])
+  end
+
+  def update
+    @subscription = Current.user.subscriptions.find(params[:id])
+    if @subscription.update(subscription_params)
+      redirect_to @subscription, notice: "Subscription was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:name, :price, :billing_frequency, :started_at, :expires_at)
+  end
 end
