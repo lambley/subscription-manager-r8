577c1be5efb408b0d2f5611d9eac335d5363fd97
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-27 11:32:33 +0000

    lint

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,18 +9,18 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       start_new_session_for @user
-      redirect_to root_path, notice: 'Successfully signed up!'
+      redirect_to root_path, notice: "Successfully signed up!"
     else
-      render :new, status: :unprocessable_entity, alert: 'Unable to sign up.'
+      render :new, status: :unprocessable_entity, alert: "Unable to sign up."
     end
   rescue ActiveRecord::RecordNotUnique
-    @user.errors.add(:email_address, 'has already been taken')
+    @user.errors.add(:email_address, "has already been taken")
     render :new, status: :unprocessable_entity
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :locale,:email_address, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :locale, :email_address, :password, :password_confirmation)
   end
 end
