2d5e8918f7eeb044eaa61f8d32b956a0d1727682
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-31 14:59:05 +0000

    Update render_button for submit buttons and use in signup and login forms

--- app/helpers/components/ui_helper.rb
+++ app/helpers/components/ui_helper.rb
@@ -3,7 +3,7 @@ module Components::UiHelper
     render partial: "components/ui/card", locals: { title: title, subtitle: subtitle, body: capture(&block) }
   end
 
-  def render_button(text:, icon: nil, link: "#", button_type: "link_to", method: nil, data: nil, bg_class: nil, **options)
+  def render_button(text:, icon: nil, link: "#", button_type: "link_to", method: nil, data: nil, bg_class: "bg-main", **options)
     render partial: "components/ui/button", locals: { text: text, icon: icon, link: link, button_type: button_type, method: method, data: data, bg_class: bg_class, options: options }
   end
 end
