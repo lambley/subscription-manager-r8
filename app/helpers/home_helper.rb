2aac6d1a0d5376f9d224712523fbc63bed365781
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-31 11:31:08 +0000

    Add card ui components with

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+  def render_card(title:, subtitle:, body:)
+    render partial: 'components/ui/card', locals: { title: title, subtitle: subtitle, body: body }
+  end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,48 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        main: "#88aaee",
+        overlay: "rgba(0,0,0,0.8)", // background color overlay for alert dialogs, modals, etc.
+
+        // light mode
+        bg: "#dfe5f2",
+        text: "#000",
+        border: "#000",
+
+        // dark mode
+        darkBg: "#272933",
+        darkText: "#eeefe9",
+        darkBorder: "#000",
+        secondaryBlack: "#212121", // opposite of plain white, not used pitch black because borders and box-shadows are that color
+      },
+      borderRadius: {
+        base: "5px",
+      },
+      boxShadow: {
+        light: "4px 3px 0px 0px #000",
+        dark: "4px 3px 0px 0px #000",
+      },
+      translate: {
+        boxShadowX: "4px",
+        boxShadowY: "3px",
+        reverseBoxShadowX: "-4px",
+        reverseBoxShadowY: "-3px",
+      },
+      fontWeight: {
+        base: "500",
+        heading: "700",
       },
     },
   },
@@ -18,5 +50,5 @@ module.exports = {
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
-  ]
-}
+  ],
+};
