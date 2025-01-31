c157254ad5e36d7a71bca0e65eaf3e0e3f2684ea
Aaron Lambley <aaron.lambley@gmail.com>
2025-01-31 12:25:48 +0000

    Create button component and helper

--- app/helpers/components/ui_helper.rb
+++ app/helpers/components/ui_helper.rb
@@ -1,5 +1,10 @@
 module Components::UiHelper
-  def render_card(title:, subtitle:, body:)
-    render partial: "components/ui/card", locals: { title: title, subtitle: subtitle, body: body }
+  def render_card(title:, subtitle:, &block)
+    render partial: 'components/ui/card', locals: { title: title, subtitle: subtitle, body: capture(&block) }
+  end
+
+  def render_button(text:, icon: nil, link: '#', button_type: 'link_to', method: nil, data: nil, bg_class: nil)
+
+    render partial: 'components/ui/button', locals: { text: text, icon: icon, link: link, button_type: button_type, method: method, data: data, bg_class: bg_class }
   end
 end
