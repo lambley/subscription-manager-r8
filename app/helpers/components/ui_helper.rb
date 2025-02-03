e2eed47fb74eec6f5065e2b62bef5f292dd0e92a
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-03 09:47:35 +0000

    Add input component and implement in search field

--- app/helpers/components/ui_helper.rb
+++ app/helpers/components/ui_helper.rb
@@ -6,4 +6,8 @@ module Components::UiHelper
   def render_button(text:, icon: nil, link: "#", button_type: "link_to", method: nil, data: nil, bg_class: "bg-main", **options)
     render partial: "components/ui/button", locals: { text: text, icon: icon, link: link, button_type: button_type, method: method, data: data, bg_class: bg_class, options: options }
   end
+
+  def render_input(name:, placeholder: nil, type: "text", value: nil, **options)
+    render partial: "components/ui/input", locals: { name: name, placeholder: placeholder, type: type, value: value, options: options }
+  end
 end
