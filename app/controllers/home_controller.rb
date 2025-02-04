f98ddc87083557a38ac8afbf3725a0c128718598
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-04 11:19:36 +0000

    Allow unauthenticated access to the about page and add a request spec for it

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  allow_unauthenticated_access only: %i[ index ]
+  allow_unauthenticated_access only: %i[ index about ]
   before_action :resume_session, only: [ :index ]
 
   def index
