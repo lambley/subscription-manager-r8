9256936b419e150b0053bededa003777f1fcea44
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-04 11:17:46 +0000

    Add an about page

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   resource :registration, only: %i[new create]
   resources :passwords, param: :token
 
+  root "home#index"
+  get "about", to: "home#about"
+
   get "edit_budget", to: "users#edit_budget"
   patch "update_budget", to: "users#update_budget"
 
-  root "home#index"
   resources :subscriptions
 end
