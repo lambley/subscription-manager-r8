d669b1ac6fe773ce04852a430985995717a8173f
Aaron Lambley <aaron.lambley@gmail.com>
2025-02-04 12:07:36 +0000

    Re-enable flash messages

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     this.element.addEventListener("click", () => this.close());
-    // setTimeout(() => this.close(), 3000);
+    setTimeout(() => this.close(), 3000);
   }
 
   close() {
